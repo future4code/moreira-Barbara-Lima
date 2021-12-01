9fc9700f7fc3b224bfe730a648e6ebfffaca8197
Barbara <b.costa.8619@gmail.com>
2021-12-01 15:11:05 -0300

    Exercícios de Strigs e Arrays

---
+++ modulo1/strigs-arrays/template_javascript/index.js
@@ -0,0 +1,86 @@
+// 1.  Indique todas as mensagens impressas no console, **SEM EXECUTAR o programa**.
+    
+//     ```jsx
+//     let array
+//     console.log('a. ', array)
+    
+//     array = null
+//     console.log('b. ', array)
+    
+//     array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//     console.log('c. ', array.length)
+    
+//     let i = 0
+//     console.log('d. ', array[i])
+    
+//     array[i+1] = 19
+//     console.log('e. ', array)
+    
+//     const valor = array[i+6]
+//     console.log**('f. ', valor)**
+//     ```
+    
+// 2. Leia o código abaixo com atenção 
+    
+//     ```jsx
+//     const frase = prompt("Digite uma frase")
+    
+//     console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+//     ```
+    
+//     Qual será o valor impresso no console se a entrada do usuário for: `"Subi num ônibus em Marrocos"`?
+
+
+// Exercício de escrita de código
+
+// Exercício 1:
+
+const nomeUsuario = prompt("Qual é o seu nome?")
+const email = prompt("e Qual é o seu email?")
+console.log("O e-mail "+email+" foi cadastrado com sucesso. Seja bem-vinda(o) " +nomeUsuario+"!")
+
+// Exercício 2:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
