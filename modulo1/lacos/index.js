7f827d822ad3473182c49222bb2d77ffd7d1185c
Barbara <b.costa.8619@gmail.com>
2021-12-08 23:36:39 -0300

    Exercícios de lacos

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -3,43 +3,58 @@
 // 1. O que o código abaixo está fazendo? Qual o resultado impresso no console?
     
 //     ```jsx
-//     let valor = 0
-//     for(let i = 0; i < 5; i++) {
-//       valor += i
-//     }
-//     console.log(valor)
+    // let valor = 0
+    // for(let i = 0; i < 5; i++) {
+    //   valor += i
+    // }
+    // console.log(valor)
 
-// 
+//  o código está somando e armazenando o o valor anterior com a posição, em um looping que para quando o contador chega 4. o resutado é 10;
 //     ```
     
 // 2. Leia o código abaixo:
     
 //     ```jsx
-//     const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-//     for (let numero of lista) {
-//       if (numero > 18) {
-//     		console.log(numero)
-//     	}
-//     }
+    // const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+    // for (let numero of lista) {
+    //   if (numero > 18) {
+    // 		console.log(numero)
+    // 	}
+    // }
 //     ```
     
 //     a) O que vai ser impresso no console?
+// 19 
+// 21
+// 23
+// 25
+// 27
+// 30
     
 //     b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
+
+// 
     
 // 3. Qual seria o resultado impresso no console, se o usuário digitasse o número `4` ?
     
+// cada linha é representada por uma *, então haveriam 4 linhas e o número de * em cada linha representa o número da linha.
+// *
+// **
+// ***
+// ****
+
+
 //     ```jsx
-//     const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
-//     let quantidadeAtual = 0
-//     while(quantidadeAtual < quantidadeTotal){
-//       let linha = ""
-//       for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-//         linha += "*"
-//       }
-//       console.log(linha)
-//       quantidadeAtual++
-//     }
+    // const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+    // let quantidadeAtual = 0
+    // while(quantidadeAtual < quantidadeTotal){
+    //   let linha = ""
+    //   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    //     linha += "*"
+    //   }
+    //   console.log(linha)
+    //   quantidadeAtual++
+    // }
     
 
 
@@ -118,20 +133,20 @@
 
 // e)
 
-const arrayOriginal = [05, 03, 06, 12, 21, 86, 92]
+// const arrayOriginal = [05, 03, 06, 12, 21, 86, 92]
 
-function imprimeMaiorMenorValor (x){
-    valorMaximo = 0
-    valorMinimo = 100
+// function imprimeMaiorMenorValor (x){
+//     valorMaximo = 0
+//     valorMinimo = 100
 
-    for(i=0; i < x.length; i++){
-        if(x[i]>valorMaximo){
-            valorMaximo=x[i]  
+//     for(i=0; i < x.length; i++){
+//         if(x[i]>valorMaximo){
+//             valorMaximo=x[i]  
             
-        }else if(x[i]<valorMinimo)
-            valorMinimo=x[i]
+//         }else if(x[i]<valorMinimo)
+//             valorMinimo=x[i]
             
-    }
-}
-imprimeMaiorMenorValor(arrayOriginal)
-console.log(`O maior valor é: ${valorMaximo},e o menor valor é: ${valorMinimo}`)
+//     }
+// }
+// imprimeMaiorMenorValor(arrayOriginal)
+// console.log(`O maior valor é: ${valorMaximo},e o menor valor é: ${valorMinimo}`)
