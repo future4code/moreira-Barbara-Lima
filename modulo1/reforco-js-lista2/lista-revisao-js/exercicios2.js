dc8f5a0b67c248c2a22b0f1e8e9b08235cd86ed6
Barbara <b.costa.8619@gmail.com>
2022-01-05 23:53:54 -0300

    reforço lista 2

--- modulo1/reforco-js-lista2/lista-revisao-js/exercicios2.js
+++ modulo1/reforco-js-lista2/lista-revisao-js/exercicios2.js
@@ -5,131 +5,96 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-
+   
     return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-    
+
     return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    
-
-    // return array.sort() 
   
 }
 
 // EXERCÍCIO 04
-
 function retornaNumerosPares(array) {
-   const arrayPar = array.filter((num) => {
-       if(num % 2 === 0)
-       return num
-     
-   })
-return arrayPar
-
-}
-
-// Desafio
-
-// let novaArray = []
-//     for(i = 0; i < array.length; i++)
-//         if(array[i] %2 === 0) {
-//             novaArray.push(array[i])
-       
-//     }
   
-//     return (novaArray)
-// }
-
-// EXERCÍCIO 05
-
-function retornaNumerosParesElevadosADois(array) {
-    const arrayParQuadrado = array.filter((num) => {
+    const novaArray = array.filter((num) =>{
         if(num % 2 === 0)
         return num
-      
-    }).map((function (num) {
-        return num*num
-    }))
-    return arrayParQuadrado
+    }
+    )
+return novaArray
 }
 
- 
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    
+    const novaArray = array.filter((num) =>{
+        if(num % 2 === 0)
+        return num
+    }).map((num) => {
+        return num*num
+    })
+return novaArray
+
+
+}
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
     let maiorNumero = -Infinity
-    for(numero of array) {
+    for(let numero of array){
         if(numero > maiorNumero){
             maiorNumero = numero
         }
-
     }
-    return maiorNumero    
-
+return maiorNumero
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-  let numeroMaior 
-  let numeroMenor
-  let maiorDivisivel
-  let diferença
+    const maiorNumero = Math.max(num1, num2)
+    const menosNumero = Math.min(num1, num2)
+    const maiorDivisivelPorMenor = maiorNumero % menosNumero === 0
+    const diferenca = Math.abs(num1 - num2)
 
-    if(num1 > num2) {
-        numeroMaior = num1
-        numeroMenor = num2
-    }else{
-        numeroMaior = num2
-        numeroMenor = num1
+    const obj = {
+        maiorNumero: maiorNumero,
+        maiorDivisivelPorMenor: maiorDivisivelPorMenor,
+        diferenca: diferenca,
     }
 
-    maiorDivisivel = numeroMaior % numeroMenor === 0
-    diferença = numeroMaior - numeroMenor
+    return obj
 
-
-
-return {
-     maiorNumero: numeroMaior, 
-     maiorDivisivelPorMenor: maiorDivisivel,
-     diferenca: diferença
-}
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-        const arrayDePares= []
-    
-        for(let i = 0; arrayDePares.length < n; i++) {
-            if(i % 2 === 0) {
-                arrayDePares.push(i)
-            }
-        }
-      return arrayDePares  
-    }
+   const arrayDePares = []
 
+   for(let i = 0; arrayDePares.length <n ; i ++ ){
+       if( i % 2 === 0){
+           arrayDePares.push(i)
+       }
+   }
+   return arrayDePares
+}
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-    if((ladoA == ladoB) && (ladoB == ladoC)) {
-        
+    if( ladoA === ladoB && ladoA === ladoC && ladoB === ladoC ){
         return ("Equilátero")
-
-    } else if((ladoA != ladoB) && ( ladoB != ladoC) && (ladoA != ladoC)) {
-   
+    } else if (ladoA !== ladoB && ladoA !== ladoC && ladoB !== ladoC) {
         return ("Escaleno")
 
     }else{
-      
         return ("Isósceles")
     }
-
 }
 
 // EXERCÍCIO 10
@@ -157,15 +122,12 @@ function retornaSegundoMaiorESegundoMenor(array) {
                 segundoMaiorNumero = numero
             }
             
-            novaArray.push(segundoMaiorNumero)
-            novaArray.push(segundoMenorNumero)
-            
-            return novaArray
+           
         }
-
-
-
-
+    novaArray.push(segundoMaiorNumero)
+    novaArray.push(segundoMenorNumero)
+            
+    return novaArray
 }
 
 // EXERCÍCIO 11
