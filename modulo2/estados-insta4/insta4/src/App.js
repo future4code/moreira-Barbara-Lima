80a08c1ee8f22adcfedd0a0c140acd4b2f84a9de
Barbara <b.costa.8619@gmail.com>
2022-01-12 22:25:42 -0300

    correção de erro no terminal

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'Paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'Barbara'}
+          fotoUsuario={'https://picsum.photos/id/1010/367/267'}
+          fotoPost={'https://picsum.photos/536/354'}
+        />
+        <Post
+          nomeUsuario={'Marcela'}
+          fotoUsuario={'https://picsum.photos/id/1011/367/267'}
+          fotoPost={'https://picsum.photos/id/0/367/267'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
